011c787bccfedd842f594066f17683664a8e0c19
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-17 20:31:15 -0800

    removing dummy image if blank

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -78,7 +78,7 @@ class Search extends Component {
                                             link={book.volumeInfo.infoLink}
                                             description={book.volumeInfo.description}
                                             // if no imageLinks then use placeholder image
-                                            image={book.volumeInfo.imageLinks === undefined ? "http://siddallheatingandcooling.net/_imgstore/5/1360415/thumbnail/FSeY96wEdX_eY4XkBN2jfYnuY9A.png" : `${book.volumeInfo.imageLinks.thumbnail}`}
+
                                         />);
                                 })}
                             </BookList>
