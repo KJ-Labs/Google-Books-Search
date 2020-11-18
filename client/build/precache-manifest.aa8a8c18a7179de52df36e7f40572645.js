d075b9219ae2513c639b249cf737ec4db75397f7
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-17 21:30:21 -0800

    Updating for screenshots and readme

---
+++ client/build/precache-manifest.aa8a8c18a7179de52df36e7f40572645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fd7dc2de6e872ab48db",
+    "url": "/static/css/main.23dec581.chunk.css"
+  },
+  {
+    "revision": "1fd7dc2de6e872ab48db",
+    "url": "/static/js/main.ccc63268.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8225d3981132153b160e",
+    "url": "/static/js/2.f57d1115.chunk.js"
+  },
+  {
+    "revision": "f6bded62697dfd7b4a4657fba3f6b44d",
+    "url": "/index.html"
+  }
+];
